cb63a59d9b13f165192f87785b79525897f72999
PatelSharanya <sharanyapatel999.sp@gmail.com>
2025-03-27 03:16:06 +0530

    allert

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from firebase_config import db
 
 app = Flask(__name__)
 
-
+# myname is swayyy
 def get_phone_numbers():
     phone_numbers = []
     users_ref = db.collection("users").stream()  # Fetch all users
